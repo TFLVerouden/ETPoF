47c4b1f0781e4b8f32cb74f5bf241ded5b6b5cd3
MightyRabbitQueen <159781648+MightyRabbitQueen@users.noreply.github.com>
2024-03-12 17:57:12 +0100

    tracks 1 particle at a time

--- HW3/PTV_oneparticle.py
+++ HW3/PTV_oneparticle.py
@@ -10,6 +10,10 @@ from scipy.optimize import curve_fit
 
 nN = 200
 
+#search neighbourhood:
+R = 7 #pixels
+Rz = 3 #allowable difference in z-coordinates between the two particles
+
 #camera A
 xA = np.zeros(0)
 zA = np.zeros(0)
@@ -24,6 +28,11 @@ x3D = np.zeros(0)
 y3D = np.zeros(0)
 z3DA = np.zeros(0)
 z3DB = np.zeros(0)
+
+#contour number
+
+N = 0
+nr = 1
 
 for i in tqdm(reversed(range(1, nN+1))):
     imgA = cv2.imread(f'PTV/a{i}.png')
@@ -46,7 +55,7 @@ for i in tqdm(reversed(range(1, nN+1))):
     cv2.drawContours(imgA, contours, -1, (0,255,0), 1)
     
     if i == 200:
-        #take only first contour
+        #take only nth contour
         #find center of countour imgA
         for j in contours:
             M = cv2.moments(j)
@@ -56,7 +65,9 @@ for i in tqdm(reversed(range(1, nN+1))):
                 z00 = M['m01']/M['m00']
                 cx = int(M['m10']/M['m00'])
                 cz = int(M['m01']/M['m00'])
-                break
+                if N == nr:
+                    break
+                N+= 1
     
         #find contours of imgB
         contours, _ = cv2.findContours(threshB, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -81,10 +92,8 @@ for i in tqdm(reversed(range(1, nN+1))):
         
     else:
         #find center of countour imgA
-        k = 0
         for j in contours:
             M = cv2.moments(j)
-            k = k + 1
             if M['m00'] != 0:
                 xA = np.append(xA, M['m10']/M['m00'])
                 zA = np.append(zA, M['m01']/M['m00'])
@@ -113,17 +122,40 @@ for i in tqdm(reversed(range(1, nN+1))):
         zpA = z3DA[-1]
         zpB = z3DB[-1]
         
-        index = np.argmin(abs(zA - zpA))
-        z3DA = np.append(z3DA, zA[index])
+        while True:
+            index = np.argmin(abs(zA - zpA))
+            #print(i, xA[index]-xp, zA[index]-zpA)
+            if np.sqrt(abs(xA[index]-xp)**2 + abs(zA[index]-zpA)**2) < R:
+                break
+            else:
+                zA = np.delete(zA, index)
+                xA = np.delete(xA, index)
+                if not len(zA) > 0:
+                    print('AAAAAAA')
         
-        #index = np.argmin(abs(xA - xp))
+        z3DA = np.append(z3DA, zA[index])
         x3D = np.append(x3D, xA[index])
         
-        index = np.argmin(abs(zB - zpB))
-        z3DB = np.append(z3DB, zB[index])
+        while True:
+            index = np.argmin(abs(zB - zpB))
+            if np.sqrt(abs(yB[index]-yp)**2 + abs(zB[index]-zpB)**2) < R:
+                if np.sqrt(abs(zB[index]-z3DA[-1])**2) < Rz:
+                    #index = np.argmin(abs(zB - zpB))
+                    z3DB = np.append(z3DB, z3DA[-1])
+                    y3D = np.append(y3D, yB[index])
+                else:
+                    #index = np.argmin(abs(zB - zpB))
+                    z3DB = np.append(z3DB, zB[index])
+                    y3D = np.append(y3D, yB[index])
+                
+                break
+            else:
+                zB = np.delete(zB, index)
+                yB = np.delete(yB, index)
+                if len(zB) == 0:
+                    print('AAAAAAA')
+                    break
         
-        #index = np.argmin(abs(yB - yp))
-        y3D = np.append(y3D, yB[index])
         
         
         
@@ -138,8 +170,9 @@ for i in tqdm(reversed(range(1, nN+1))):
     
     cv2.circle(imgA, (int(x3D[-1]), int(z3DA[-1])), 4, (0, 250, 250), 1)
     cv2.circle(imgB, (int(y3D[-1]), int(z3DB[-1])), 4, (0, 250, 250), 1)
-    cv2.line(imgB, (0, int(z3DB[-1])), (450, int(z3DB[-1])), (255, 250, 250), 1)
-    
+    #cv2.line(imgB, (0, int(z3DB[-1])), (450, int(z3DB[-1])), (255, 250, 250), 1)
+    #cv2.line(imgB, (0, int(z3DA[-1])), (450, int(z3DA[-1])), (255, 0, 0), 1)
+    '''
     plt.figure(1); plt.clf()
     plt.imshow(imgA[int(z3DA[-1])-50:int(z3DA[-1])+50,:])
     plt.pause(0.01)
@@ -147,6 +180,8 @@ for i in tqdm(reversed(range(1, nN+1))):
     plt.figure(2); plt.clf()
     plt.imshow(imgB[int(z3DA[-1])-50:int(z3DA[-1])+50,:])
     plt.pause(0.01)
+    '''
+    
     
 
 
@@ -159,16 +194,21 @@ plt.scatter(xB, yB)
 '''
 
 # Creating figure
-fig = plt.figure(figsize = (10, 7))
+fig = plt.figure()
 ax = plt.axes(projection ="3d")
- 
+ax.set_box_aspect((450, 450, 860))
+col = np.arange(len(x3D))
+cm = plt.get_cmap("RdYlGn")
+
 # Creating plot
-ax.scatter3D(x3D, y3D, z3DA, color = "green", alpha = 0.5, s = 10)
-ax.scatter3D(x3D, y3D, z3DB, color = "blue", alpha = 0.5, s = 10)
+ax.plot(x3D, y3D, z3DA, color = "green", alpha = 0.5)
+#ax.scatter3D(x3D, y3D, z3DB, color = "blue", alpha = 0.5, s = 10)
 plt.title("simple 3D scatter plot")
 ax.set_xlabel('X-axis', fontweight ='bold') 
 ax.set_ylabel('Y-axis', fontweight ='bold') 
 ax.set_zlabel('Z-axis', fontweight ='bold')
- 
+ax.set_xlim(0, 450)
+ax.set_ylim(0, 450)
+ax.set_zlim(0, 860)
 # show plot
 plt.show()
