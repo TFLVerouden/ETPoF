89a5e2504216d214118d223cfdd545f13477fc17
CtrlAltOmega <tommie.verouden@gmail.com>
2024-04-04 11:54:50 +0200

    Fixed arrow directions in plots

--- HW4/piv_functions.py
+++ HW4/piv_functions.py
@@ -87,8 +87,7 @@ def correlate_image_pair(image0, image1, method='correlate', plot=False):
     return correlation
 
 
-def find_displacement(correlation, subpixel_method='gauss_neighbor',
-                      plot=False):
+def find_displacement(correlation, subpixel_method='gauss_neighbor'):
     """
     TODO: Add documentation
     """
@@ -107,11 +106,15 @@ def find_displacement(correlation, subpixel_method='gauss_neighbor',
         # Take the first value if only one peak was found
         peak = peak[0]
 
+    # If the peak is at the edge of the correlation array...
+    if np.any(peak == np.array(correlation.shape - np.ones_like((1, 2)))):
+        # Throw a warning
+        print('Peak is at the edge of the correlation array')
+
     # If the subpixel option was set...
-    if subpixel_method is not None:
+    elif subpixel_method is not None:
         # Refine the peak location
-        correction = subpixel_refinement(correlation, peak, subpixel_method,
-                                         plot=plot)
+        correction = subpixel_refinement(correlation, peak, subpixel_method)
         peak = peak + np.array(correction)
 
     # Subtract the image center to get relative coordinates
@@ -121,7 +124,7 @@ def find_displacement(correlation, subpixel_method='gauss_neighbor',
     return displacement
 
 
-def subpixel_refinement(correlation, peak, method='gauss_neighbor', plot=False):
+def subpixel_refinement(correlation, peak, method='gauss_neighbor'):
     """
     TODO: Add documentation
     """
@@ -129,16 +132,19 @@ def subpixel_refinement(correlation, peak, method='gauss_neighbor', plot=False):
     # Three-point offset calculation from the lecture
     if method == 'gauss_neighbor':
 
-        # TODO: This is janky
-        # Zero-pad the correlation array by two pixels in each direction
-        correlation = np.pad(correlation, 2, mode='constant', constant_values=0)
-
         # Get the neighbouring pixels in both dimensions
         neighbors = [correlation[(peak[0] - 1):(peak[0] + 2), peak[1]],
                      correlation[peak[0], (peak[1] - 1):(peak[1] + 2)]]
 
+        # If the neighbors shape is not 3x3...
+        if not all([neighbor.shape == (3,) for neighbor in neighbors]):
+            fig, ax = plt.subplots()
+            ax.imshow(correlation, cmap='gray')
+            ax.plot(peak[1], peak[0], 'ro')
+            plt.show()
+
         # Change all zeros to a small value to avoid division by zero
-        neighbors = [np.where(neighbor == 0, 1, neighbor) for neighbor in neighbors]
+        # neighbors = [np.where(neighbor == 0, 1, neighbor) for neighbor in neighbors]
 
         # Three-point Gaussian fit in both dimensions
         correction = [(0.5 * (np.log(neighbor[0]) - np.log(neighbor[2]))
@@ -148,39 +154,233 @@ def subpixel_refinement(correlation, peak, method='gauss_neighbor', plot=False):
     else:
         raise ValueError('Invalid method')
 
-    # If the correction contains nans...
-    if np.any(np.isnan(correction)):
-        # Error
-        # raise ValueError('Subpixel refinement failed')
-        pass
-
     return correction
 
 
-def divide_in_windows(image, window_size, overlap=0):
+def assume_squareness(size):
+    """
+
+    PARAMETERS:
+        size (int or tuple): Size of a window in pixels. If an integer is
+            supplied, the function turns this into a square window.
+
+    RETURNS:
+        size (np.array): Size of a window in pixels, as a 1D, 2-element array.
+    """
+
+    # Turn int or tuple into 1D array
+    size = np.array([size]).flatten()
+
+    # If the array has more than two elements, raise an error
+    if size.size > 2:
+        raise ValueError('The size should be a 1D array with 1 or 2 elements.')
+
+    # If the size is a single element, assume a square window
+    if len(size) == 1:
+        size = np.array([size[0], size[0]])
+
+    return size
+
+
+def divide_in_windows(images, window_size):
     """
     TODO: Add documentation
     """
 
-    # Check whether the image can be evenly divided into windows of this size
-    if not np.all([np.mod(image.shape[j], window_size[j]) == 0 for j in
-                   range(len(image.shape))]):
+    # Process window_size, which may be an integer or tuple
+    window_size = assume_squareness(window_size)
+
+    # Check whether one or multiple images was supplied
+    if len(images.shape) == 2:
+        # If only one image was supplied, add a dimension
+        images = images[np.newaxis, :, :]
+
+    # Check whether the images can be evenly divided into windows of this size
+    if not np.all([np.mod(images.shape[j + 1], window_size[j]) == 0 for j in
+                   range(len(images.shape) - 1)]):
         # Error
-        raise ValueError(f'A {window_size} window does not fit into the image.')
+        raise ValueError(
+            f'A {window_size} window does not fit into the images.')
 
     # Get the coordinates of the top left pixel of each window
-    coordinates = np.array([[[y, x] for y in range(0, image.shape[0],
-                                                   window_size[0])]
-                            for x in range(0, image.shape[1], window_size[1])])
+    coordinates = np.array(
+            [[[y, x] for y in range(0, images.shape[1], window_size[0])] for x
+             in range(0, images.shape[2], window_size[1])])
 
-    # Divide the image into windows of size window_size
-    windows = np.array([image[y:(y + window_size[0]), x:(x + window_size[1])]
-                        for y in range(0, image.shape[0], window_size[0]) for x
-                        in range(0, image.shape[1], window_size[1])])
+    # Get the coordinates of the centre of each window
+    coordinates = coordinates + np.array(window_size) / 2
 
-    # Reshape the windows array to be 4D
-    windows = windows.reshape((image.shape[0] // window_size[0],
-                               image.shape[1] // window_size[1], window_size[0],
-                               window_size[1]))
+    # Divide the images into windows of size window_size
+    windows = np.array(
+            [[[images[z, y:(y + window_size[0]), x:(x + window_size[1])]
+               for x in range(0, images.shape[2], window_size[1])] for y in
+              range(0, images.shape[1], window_size[0])] for z in
+             range(images.shape[0])])
 
     return windows, coordinates
+
+
+def simple_piv(images, window_size, calib_dist=None, calib_time=None,
+               subpixel_method='gauss_neighbor', plot=False,
+               plot_flow_field_params={}, plot_displacements_params={}):
+    """
+    TODO: Add documentation
+    """
+
+    # Check whether there is two or more images
+    if images.shape[0] < 2:
+        # Error
+        raise ValueError('At least two images are required for PIV.')
+    elif images.shape[0] > 2:
+        # Warning
+        raise NotImplementedError('Only two images are currently supported.')
+
+    # Divide the images into windows
+    windows, coordinates = divide_in_windows(images, window_size)
+
+    # Calculate the correlation of each window j,i in frame 0 with the
+    # corresponding window in frame 1
+    correlations = np.array([[correlate_image_pair(windows[0, j, i],
+                                                   windows[1, j, i])
+                              for i in range(len(windows[0, 0]))]
+                             for j in range(len(windows[0, 1]))])
+
+    # Calculate the displacement of each window j,i in frame 0 with the
+    # corresponding window in frame 1
+    displacements = np.array(
+            [[find_displacement(correlation, subpixel_method=subpixel_method)
+              for correlation in row] for row in correlations])
+
+    if plot:
+        # Plot the flow field
+        plot_flow_field(displacements, coordinates, window_size,
+                        **plot_flow_field_params)
+
+        # Plot the displacements
+        plot_displacements(displacements)
+        pass
+
+    # Calculate the velocity field
+    if calib_dist is not None and calib_time is not None:
+        # Calibrate the displacement vectors
+        displacements_calibrated = displacements * calib_dist
+
+        # Calculate the velocity vectors
+        velocities = displacements_calibrated / calib_time
+
+        return velocities, coordinates
+    else:
+        return displacements, coordinates
+
+
+def plot_flow_field(displacements, coordinates, window_size,
+                    plot_windows=True, arrow_scale=1, calib_dist=None,
+                    units=None, title='Flow field'):
+    """
+    TODO: Add documentation
+    """
+
+    # Assume the window size is square
+    window_size = assume_squareness(window_size)
+
+    # Plot all displacement vectors at the center of each window
+    fig, ax = plt.subplots()
+
+    # If a calibration distance was specified, calibrate all values
+    if calib_dist is not None:
+        displacements = displacements * calib_dist
+        coordinates = coordinates * calib_dist
+        window_size = window_size * calib_dist
+
+    # Show a grid with the outline of each window and an arrow in the centre
+    # indicating the displacement
+    arrow_param = calib_dist if calib_dist is not None else 1
+
+    for j in range(len(coordinates[0])):
+        for i in range(len(coordinates[1])):
+            # Calculate the start and end of the arrow
+            arrow_start = np.array([coordinates[j, i][1] + window_size[1] / 2 -
+                                    arrow_scale * 0.5 * displacements[j, i][0],
+                                    coordinates[j, i][0] + window_size[0] / 2 -
+                                    arrow_scale * 0.5 * displacements[j, i][1]])
+
+            # Plot the arrow
+            ax.arrow(arrow_start[1], arrow_start[0],
+                     arrow_scale * displacements[j, i][1],
+                     arrow_scale * displacements[j, i][0],
+                     width=1.5 * arrow_param,
+                     head_width=10 * arrow_param,
+                     head_length=7 * arrow_param,
+                     fc='k', ec='k', lw=1)
+
+            # Plot the window
+            if plot_windows:
+                ax.add_patch(plt.Rectangle((coordinates[j, i][1],
+                                            coordinates[j, i][0]),
+                                           window_size[1],
+                                           window_size[0], fill=None,
+                                           edgecolor='darkgrey',
+                                           linewidth=1))
+
+    # Aspect ratio should be 1
+    ax.set_aspect('equal')
+
+    # Flip y axis
+    ax.invert_yaxis()
+
+    # If a calibration distance was specified, add units to the labels
+    if calib_dist is not None:
+        ax.set_xlabel(f'x [{units}]')
+        ax.set_ylabel(f'y [{units}]')
+    else:
+        ax.set_xlabel('x [px]')
+        ax.set_ylabel('y [px]')
+
+    # If a arrow scale was specified, add it to the title
+    if arrow_scale != 1:
+        title = title + f' (arrows ×{arrow_scale})'
+    ax.set_title(title)
+    plt.show()
+
+    return fig, ax
+
+
+def plot_displacements(displacements, calib_dist=None, units=None,
+                       title='Displacement vectors'):
+    """
+    TODO: Add documentation
+    """
+
+    # If a calibration distance was specified, calibrate all values
+    if calib_dist is not None:
+        displacements = displacements * calib_dist
+
+    # Plot all displacement vectors
+    fig, ax = plt.subplots()
+    ax.plot(displacements[:, :, 1].flatten(), displacements[:, :, 0].flatten(),
+            'o', color='black', markersize=2)
+
+    # Draw zero lines
+    ax.axhline(0, color='black', lw=0.5)
+    ax.axvline(0, color='black', lw=0.5)
+
+    # Pad the limits
+    ax.set_xlim([np.amin(displacements[:, :, 1]) - 1,
+                 np.amax(displacements[:, :, 1]) + 1])
+    ax.set_ylim([np.amin(displacements[:, :, 0]) - 1,
+                 np.amax(displacements[:, :, 0]) + 1])
+
+    ax.set_aspect('equal')
+
+    # If a calibration distance was specified, add units to the labels
+    if calib_dist is not None:
+        ax.set_xlabel(f'Δx [{units}]')
+        ax.set_ylabel(f'Δy [{units}]')
+    else:
+        ax.set_xlabel('Δx [px]')
+        ax.set_ylabel('Δy [px]')
+
+    ax.set_title(title)
+    plt.show()
+
+    return fig, ax
